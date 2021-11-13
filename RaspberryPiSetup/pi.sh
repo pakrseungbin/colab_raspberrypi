e01075f5991f808c7f32072ffb05778a4eac4fc5
LEE-SEON-WOO <mapomskrcolab@gmail.com>
2021-11-13 17:58:59 +0900

    sh file typo modified

--- RaspberryPiSetup/pi.sh
+++ RaspberryPiSetup/pi.sh
@@ -4,13 +4,11 @@
 #sudo raspi-config
 
 sudo apt update
-sudo apt install git vim python3-pip libwebp-dev libtiff5 libopencv-dev libatlas-base-bev libopenblas-dev libgtk2.0-dev -y
+sudo apt install git vim python3-pip libwebp-dev libtiff5 libopencv-dev libatlas-base-dev libopenblas-dev libgtk2.0-dev -y
 pyhton3 -m pip install torch-1.7.0a0-cp37-cp37m-linux_armv7l.whl
 python3 -m pip install torchvision-0.8.0a0+45f960c-cp37-cp37m-linux_armv7l.whl
-python3 -m pip install opencv-python-headless=4.4.0.40
+python3 -m pip install opencv-python-headless==4.4.0.40
 sed -i 's/torchvision>=0.8.1/#torchvision>=0.8.1/g' requirements.txt
 sed -i 's/opencv-python>=4.1.2/#opencv-python>=4.1.2/g' requirements.txt
 python3 -m pip install -r requirements.txt
 
-###To start the software
-#python3 detect.py --weights YOLOv5-Lite-best.pt --source 0
